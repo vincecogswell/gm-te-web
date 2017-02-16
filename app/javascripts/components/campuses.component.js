9f07b40e676de99b6c5deee958b25cf6870cb9c3
Team GM <cse498@cse498-0601-gm.cse.msu.edu>
2017-02-16 17:59:13 -0500

    adding showMap function that is triggered by clicking on an Edit button

--- app/javascripts/components/campuses.component.js
+++ app/javascripts/components/campuses.component.js
@@ -3,21 +3,25 @@
 
     angular
         .module('app.campuses', ['ngRoute'])
-        .config(['$routeProvider', function($routeProvider) {
+        .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/campuses', {
                 templateUrl: 'partials/components/campuses.component.html',
                 bindToController: true,
                 controllerAs: 'vm',
                 controller: 'CampusesCtrl',
                 resolve: {
-                    campuses: function(campusService) {
+                    campuses: function (campusService) {
                         return campusService.getCampuses();
                     }
                 }
             });
         }])
-        .controller('CampusesCtrl', ['campuses', function(campuses) {
+        .controller('CampusesCtrl', ['campuses', function (campuses) {
             var self = this;
             self.campuses = campuses;
+
+            self.showMap = function (campus) {
+                
+            }
         }]);
 })();
