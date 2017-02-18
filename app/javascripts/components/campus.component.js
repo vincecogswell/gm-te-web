d3130d6c5f8200d852e4c76e74ad2a99516e33f3
Brendan Carlson <brendancarlson@Brendans-MacBook-Pro.local>
2017-02-18 02:28:29 -0500

    starting to integrate Google Maps functionalities

--- app/javascripts/components/campus.component.js
+++ app/javascripts/components/campus.component.js
@@ -35,6 +35,29 @@
             self.lots = self.campus ? lotService.getLotsOnCampus(campusId) : null;
             self.gates = self.campus ? gateService.getGatesOnCampus(campusId) : null;
 
+            self.createMap = function () {
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 42.5122427, lng: -83.0334234},
+                    zoom: 10
+                });         
+                var drawingManager = new google.maps.drawing.DrawingManager({
+                    drawingControl: true,
+                    drawingControlOptions: {
+                        position: google.maps.ControlPosition.TOP_CENTER,
+                        drawingModes: ['rectangle', 'polygon']
+                    },
+                    rectangleOptions: {
+                        draggable: true,
+                        editable: true
+                    },
+                    polygonOptions: {
+                        draggable: true,
+                        editable: true
+                    }
+                });
+                drawingManager.setMap(map);
+            }
+
             self.goBack = function () {
                 $location.path('/campuses');
             }
