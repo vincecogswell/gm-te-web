d39b8cdc49ddc5fd409f2a0962ca02b627c2b566
root <root@cse498-2017-1-team-gm.cse.msu.edu>
2017-02-26 15:23:16 -0500

    is this a separate repo?

--- app/src/services/campus.service.js
+++ app/src/services/campus.service.js
@@ -4,12 +4,56 @@
 	angular
 		.module('app').factory('campusService', campusService);
 
-	function campusService() {
+	campusService.$inject = ['$http', '$q'];
+
+	function campusService($http, $q) {
+
+		var _campusesPromise;
 
 		function getCampuses() {
-			// TO DO: API call
+			if (!_campusesPromise) {
+				var deferred = $q.defer();
+				$http.get("/campus/1/gates")
+				.then( function (response) {
+					console.log(response);
+					deferred.resolve(response);
+				});
+
+				_campusesPromise = deferred.promise;
+			}
+
             return campuses;
+			//return _campusesPromise;
 		}
+
+        var _usersPromise;
+
+        function getUsers() {
+            if(!_usersPromise) {          
+                var deferred = $q.defer();
+                var username = '';
+                if (simulatedUser) {
+                    username = simulatedUser.username;
+                }
+                Users.query({ username: username }, function (allUsers) {
+                    var windowsUsername = allUsers[1];
+                    users = Object.keys(allUsers[0]).map(function (key) { return allUsers[0][key] } ); // converting object to array
+
+                    users.forEach(function(user, index) {
+                        if (!simulatedUser && user.username === windowsUsername) {
+                            curUser = user;
+                        }
+                        user.projects = user.projects.map(projectsService.normalizeProjectFromDB);
+                    });
+                                               
+                    deferred.resolve(users);
+                });
+                
+                _usersPromise = deferred.promise;
+            }
+
+            return _usersPromise;
+        }
 
 		function getCampus(campusId) {
 			for (var _campusId in campuses) {
