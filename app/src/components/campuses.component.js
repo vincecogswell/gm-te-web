254c8a96895db559f70f5bca86e0776546d2ba0a
Brendan Carlson <brendancarlson@Brendans-MacBook-Pro.local>
2017-03-08 16:01:05 -0500

    some changes

--- app/src/components/campuses.component.js
+++ app/src/components/campuses.component.js
@@ -78,9 +78,13 @@
                     perimeter.push(event.overlay.getBounds().getNorthEast());
                     perimeter.push(event.overlay.getBounds().getSouthWest());
                 } else if (event.type == 'polygon') {
-                    perimeter = event.overlay.getPaths();
+                    var paths = event.overlay.getPaths();
+                    for (var i = 0; i < paths.getLength(); i++) {
+                        perimeter.push(paths.getAt(i));
+                    }
                 }
                 console.log(perimeter.getArray());
+                console.log(perimeter.getArray()[0].lat);
             });
 
             // Create the search box and link it to the UI element.
