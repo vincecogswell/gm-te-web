27374239e5a7447df6af88588b185f4ff47a356e
Brendan Carlson <brendancarlson@Brendans-MacBook-Pro.local>
2017-03-13 02:06:54 -0400

    hopefully fixing another bug

--- app/src/services/map.service.js
+++ app/src/services/map.service.js
@@ -6,17 +6,25 @@
 
 	function mapService() {
 
-        function convertToGMPoint(point) {
-            return new google.maps.LatLng(point['lat'], point['lng']);
+        function convertToGMCoord(coord) {
+            coord['lat'] = Number(coord['lat']);
+            coord['lng'] = Number(coord['lng']);
+            return new google.maps.LatLng(coord['lat'], coord['lng']);
         }
 
         function convertToGMPolygon(perimeter) {
             var bounds = new google.maps.LatLngBounds();
-            for (var key in perimeter) {
+            /*for (var key in perimeter) {
                 if (perimeter.hasOwnProperty(key)) {
                     let point = perimeter[key];
                     bounds.extend(new google.maps.LatLng(point['lat'], point['lng']));
                 }
+            }*/
+            for (var i = 0; i < perimeter.length; i++) {
+                let coord = perimeter[i];
+                coord['lat'] = Number(coord['lat']);
+                coord['lng'] = Number(coord['lng']);
+                bounds.extend(new google.maps.LatLng(coord['lat'], coord['lng']));            
             }
             return bounds;
         }
