ab2fadcb014962af4b0c0bcebbb60917765c5f54
Brendan Carlson <brendancarlson@Brendans-MacBook-Pro.local>
2017-03-22 23:03:38 -0400

    seeing if this works

--- app/src/components/campus.component.js
+++ app/src/components/campus.component.js
@@ -804,7 +804,7 @@
                     drawingManagerGate.setOptions({
                         drawingControl: false
                     });
-                    $scope.$apply();
+                    //$scope.$apply();
                 }
             });
 
