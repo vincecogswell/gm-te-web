f6025d9ecad69689d8fe8b567b6b989519bbf1e8
Brendan Carlson <brendancarlson@host-147392.dhcp.egr.msu.edu>
2017-03-23 20:43:53 -0400

    dsakj

--- app/src/services/campus.service.js
+++ app/src/services/campus.service.js
@@ -17,7 +17,7 @@
 					console.log(response);
 					if (response && response.data && response.data.status === 200) {
 						campuses = response.data.campuses;
-						$http.get('/predictiveparking/1')
+						$http.get('/predictive-parking/1')
 						.then( function (response) {
 							console.log(response);
 						});
