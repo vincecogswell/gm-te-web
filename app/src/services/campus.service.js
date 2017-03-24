6e509a6742a632acdd772019f0cd43b32eae99fe
Brendan Carlson <brendancarlson@Brendans-MacBook-Pro.local>
2017-03-23 22:52:41 -0400

    adskj

--- app/src/services/campus.service.js
+++ app/src/services/campus.service.js
@@ -17,10 +17,10 @@
 					console.log(response);
 					if (response && response.data && response.data.status === 200) {
 						campuses = response.data.campuses;
-						$http.get('/predictive-parking/1/1')
+						/*$http.get('/predictive-parking/1/1')
 						.then( function (response) {
 							console.log(response);
-						});
+						});*/
 						next(campuses);
 					} else {
 						// error
